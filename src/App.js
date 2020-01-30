b593074bdc8938ca61b882dfbbb65ae114898a59
BIBIN OOMMEN MATHEW <bibin765@gmail.com>
2020-01-30 21:25:11 +0530

    second

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MyComponent from './typist';
 class App extends Component{
   render(){
     return(
-      <div>
+      <div className="container">
         <MyComponent />
       </div>
     )
