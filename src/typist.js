f5eb00e378825e14bfeda4d15c66f1ddd4704f83
Bibin Mathew <bibin765@gmail.com>
2020-03-25 09:24:03 +0530

    Update typist.js

--- src/typist.js
+++ src/typist.js
@@ -12,9 +12,14 @@ export default class MyComponent extends Component {
         <br />
         <br />
         
-        {<h6>Whether you’re planning something,
+        {<h6>Are you planning something,
           </h6>}
-        {<h4>Have a smooth and successful planning experience @</h4>}
+        {<h4>Are you a dreamer?
+Do you always wanted to achieve something?
+Anytime you planned something but lose it on halfway?
+You are in the right Place..
+We will be your proper mentor..
+We will make you to learn,do and create something new @</h4>}
         <Typist.Delay ms={1000}/>
             <h1>Sainy.live</h1>
             instagram.com/sainyLive
