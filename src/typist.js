006a3e3ebe52ca54934d86ed53bd4523c1646705
Bibin Mathew <bibin765@gmail.com>
2020-03-25 09:21:47 +0530

    Update typist.js

--- src/typist.js
+++ src/typist.js
@@ -12,13 +12,12 @@ export default class MyComponent extends Component {
         <br />
         <br />
         
-        {<h6>Whether you’re releasing a digital product,a new makeup collection, a limited-edition sneaker, or a popular collectible, you’re focused on a pre-launch buzz campaign. But if you overlook how your website or app will handle the hype, you’d make a serious error.
+        {<h6>Whether you’re planning something,
           </h6>}
-        {<h4>Have a smooth and successful product launch experience @</h4>}
+        {<h4>Have a smooth and successful planning experience @</h4>}
         <Typist.Delay ms={1000}/>
             <h1>Sainy.live</h1>
-            Coming Soon
-        <Typist.Backspace count={11} delay={200} />
+            instagram.com/sainyLive
       </Typist>
       
       </div>
